530b8f8ae3da83f4ef8c420c896225b24dbd2c4c
Umbra-Ignis <devon.j.sanders@gmail.com>
2018-04-17 15:18:45 +1000

    Finished Main Menu Added Menu Icons Added MenuManager script

---
+++ Game Jam 16-04/Assets/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject controlsParent;
+
+	[HideInInspector]
+	public bool controlsActive = false;
+
+	public void Controls()
+	{
+		if (controlsActive == false) 
+		{
+			controlsParent.SetActive (true);
+			controlsActive = true;
+		}
+		else
+		{
+			controlsActive = false;
+			controlsParent.SetActive (false);
+		}
+	}
+
+	public void Play()
+	{
+		SceneManager.LoadScene (sceneBuildIndex: 1);
+	}
+}
